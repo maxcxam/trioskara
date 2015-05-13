00f8d201325276de28d47270e9039d6f00089a1b
maxcxam <maxcxam1979@gmail.com>
2015-05-13 15:50:15 -0500

    <commit message>

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$linkid = mysql_connect('mysql.hostinger.com.ua','u180503935_root','Ghbdtngh1') or die (mysql_error());
+$linkid = mysql_connect('localhost','root','Ghbdtngh1') or die (mysql_error());
 mysql_set_charset('utf8',$linkid);
 //$charset = mysql_client_encoding($linkid);
-mysql_select_db('u180503935_kli');
+mysql_select_db('kli');
 $text=file_get_contents("congratulations.ini");
 
 
